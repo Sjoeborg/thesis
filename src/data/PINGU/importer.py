b2f3911dd0ac085579227a6db1626e31fe6e5ef3
Martin Sjöborg <martin@sjoborg.org>
2021-04-13 15:22:40 +0200

    update

--- src/data/PINGU/importer.py
+++ src/data/PINGU/importer.py
@@ -10,6 +10,8 @@ from DC.processer import get_flux, interpolate_flux_DC #DC flux can be used
 
 def MC_PINGU():
     df = pd.read_csv(f'./src/data/files/PINGU/neutrino_mc.csv', dtype=np.float64)
+    df['reco_coszen'] = np.cos(df['reco_zenith'])
+    df['true_coszen'] = np.cos(df['true_zenith'])
     return df
 
 
